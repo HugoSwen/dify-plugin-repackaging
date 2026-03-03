e0564ab6c60f9854844ed1f2b0bc295e2a56f017
王雨乐（Wang Yule） <113964262+HugoSwen@users.noreply.github.com>
2026-03-03 16:27:39 +0800

    Add test echo statement in plugin_repackaging.sh
    Added a test echo statement before pip download command.

--- plugin_repackaging.sh
+++ plugin_repackaging.sh
@@ -110,6 +110,7 @@ repackage(){
 	echo "Unzip success."
 	echo "Repackaging ..."
 	cd ${CURR_DIR}/${PACKAGE_NAME}
+	echo "TEST........................"
 	pip download ${PIP_PLATFORM} -r requirements.txt -d ./wheels --index-url ${PIP_MIRROR_URL} --trusted-host mirrors.aliyun.com
 	if [[ $? -ne 0 ]]; then
 		echo "Pip download failed."
